5c4e89b2c868c43f5ebf3334d84bc6f6376c4dbd
Julian Tato <jutato141098@gmail.com>
2025-10-24 15:57:44 -0300

    creacion

---
+++ Dominio/Articulos/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Articulos
+{
+    internal class Articulo
+    {
+        public int IDArticulo { get; set; }
+        public string CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Categorias Categorias { get; set; }
+        public Marcas Marca { get; set; }
+        public decimal Precio { get; set; }
+        public Imagenes Imagen { get; set; }
+
+        public List<string> Imagenes { get; set; } = new List<string>();
+    }
+}
---
+++ Dominio/Articulos/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Articulos
+{
+    internal class Categoria
+    {
+        public int IDCategoria { get; set; }
+        public string descripcion { get; set; }
+        public bool estado { get; set; }
+        public override string ToString()
+        {
+            return descripcion;
+        }
+    }
+}
